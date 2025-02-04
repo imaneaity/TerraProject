7fcb676fee1b85ad1b140e4b21fb04a9e9ac89eb
imaneaity <imaneaity>
2025-02-04 22:50:45 +0000

    integrating modules

--- webapp_module/variables.tf
+++ webapp_module/variables.tf
@@ -6,12 +6,24 @@ variable "region" {
   default     = "eu-west-2"
 }
 
+variable "app_name" {
+  description = "Name of the web application"
+  type        = string
+  default     = "web-app"
+}
+
+variable "environment_name" {
+  description = "Deployment environment (dev/staging/production)"
+  type        = string
+  default     = "dev"
+}
+
 # EC2 Variables
 
 variable "ami" {
   description = "Amazon machine image to use for ec2 instance"
   type        = string
-  default     = "ami-04ba8620fc44e2264" # Ubuntu 20.04 LTS // eu-west-2
+  default     = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
 }
 
 variable "instance_type" {
@@ -29,10 +41,19 @@ variable "bucket_prefix" {
 
 # Route 53 Variables
 
+variable "create_dns_zone" {
+  description = "If true, create new route53 zone, if false read existing route53 zone"
+  type        = bool
+  default     = false
+}
+
+/*
 variable "domain" {
   description = "Domain for website"
   type        = string
 }
+*/
+
 
 # RDS Variables
 
